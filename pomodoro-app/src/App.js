db62889c968f2ff37cac32a345cf9f457a10058f
Ario Bintang <ariobintangn@gmail.com>
2023-03-01 11:50:48 +0700

    initial commit

---
+++ pomodoro-app/src/App.js
@@ -0,0 +1,12 @@
+import Timer from './Timer.js'
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import './App.css';
+
+function App() {
+  return (
+    <main>test</main>
+  );
+}
+
+export default App;
---
+++ pomodoro-app/src/Timer.js
@@ -0,0 +1,5 @@
+function Timer(){
+    <div>Timer</div>
+}
+
+export default Timer;
