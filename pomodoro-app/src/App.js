4ee899a7313546ffffabd40b10b57f24e869b797
Ario Bintang <ariobintangn@gmail.com>
2023-03-01 13:14:26 +0700

    creating buttons

--- pomodoro-app/src/App.js
+++ pomodoro-app/src/App.js
@@ -1,11 +1,16 @@
-import Timer from './Timer.js'
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
-import './App.css';
+import Timer from "./Timer.js";
+import Settings from './Settings.js'
+import "./App.css";
+import {useState} from 'react'
 
 function App() {
+
+  const [showSettings, setShowSettings] = useState(initialState: true);
   return (
-    <main>test</main>
+    <main>
+ 
+      {showSettings ? <Settings /> : <Timer />}
+    </main>
   );
 }
 
--- pomodoro-app/src/Timer.js
+++ pomodoro-app/src/Timer.js
@@ -1,5 +1,35 @@
-function Timer(){
-    <div>Timer</div>
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import PauseButton from "./PauseButton.js";
+import PlayButton from "./PlayButton.js";
+import SettingsButton from './SettingsButton.js'
+
+const red = "#f54e4e";
+const green = "#4aec8c";
+
+function Timer() {
+  return (
+    <div>
+      <CircularProgressbar
+        value={60}
+        text={`60%`}
+        styles={buildStyles({
+          rotation: 1,
+          strokeLinecap: 1,
+          textColor: "fff",
+          pathColor: red,
+          trailColor: "rgba(255,255,255,.2",
+        })}
+      />
+      <div style={{ marginTop: "20px" }}>
+        <PlayButton />
+        <PauseButton />
+      </div>
+      <div styles={{ marginTop: "20px;" }}>
+        <SettingsButton />
+      </div>
+    </div>
+  );
 }
 
 export default Timer;
